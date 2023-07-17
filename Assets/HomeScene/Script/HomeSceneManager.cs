d257d8611c97af5bf04a10396287147a65123a3e
Vinit532 <93425465+Vinit532@users.noreply.github.com>
2023-07-17 13:50:16 +0530

    Home Scene Created

---
+++ Assets/HomeScene/Script/HomeSceneManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomeSceneManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private void OnGUI()
+    {
+        Screen.orientation = ScreenOrientation.Portrait;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
